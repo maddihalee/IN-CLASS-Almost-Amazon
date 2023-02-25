9285cb13beec8374dcb7beb0d5945a376510be56
Maddi Cook <maddihcook@gmail.com>
2023-02-25 12:41:03 -0600

    added merged data

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,22 @@
+import { getSingleBook, deleteBook } from './bookData';
+import { getSingleAuthor, deleteSingleAuthor, getAuthorBooks } from './authorData';
+
 // for merged promises
+const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+  getSingleBook(firebaseKey).then((bookObject) => {
+    getSingleAuthor(bookObject.author_id)
+      .then((authorObject) => resolve({ ...bookObject, authorObject }));
+  }).catch(reject);
+});
+
+const deleteAuthorBooksRelationship = (firebaseKey) => new Promise((resolve, reject) => {
+  getAuthorBooks(firebaseKey).then((authorBooksArray) => {
+    const deleteBookPromises = authorBooksArray.map((book) => deleteBook(book.firebaseKey));
+
+    Promise.all(deleteBookPromises).then(() => {
+      deleteSingleAuthor(firebaseKey).then(resolve);
+    });
+  }).catch(reject);
+});
+
+export { getBookDetails, deleteAuthorBooksRelationship };
